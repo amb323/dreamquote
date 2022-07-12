372de418f95e2f574b0c1a18d5d8092144a5a17b
amb323 <88760050+amb323@users.noreply.github.com>
2022-07-12 16:14:14 -0300

    Changes #1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import quote from './data.json'
 
 function App() {
+  const x = Math.floor(Math.random() * 10);
+  console.log(x);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <h1 className="title">Dream Quote</h1>
+      <p className="texto">{quote[x].quote}</p>
+      <div className="buttonbox">
+      <button className="boton" onClick={() => window.location.reload()}>NEW QUOTE</button>
+      </div>
+      </>
   );
 }
 

ba6da5f978edf37079c5f23abae68cb2e06baf61
amb323 <88760050+amb323@users.noreply.github.com>
2022-07-12 18:39:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   const x = Math.floor(Math.random() * 10);
   return (
       <>
+      <div className="imagebox">
+      <img src={logo} className="image" />
+      </div>
       <h1 className="title">Dream Quote</h1>
       <p className="texto">{quote[x].quote}</p>
       <div className="buttonbox">
